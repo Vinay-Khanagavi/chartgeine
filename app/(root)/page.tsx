c5e077e365c7a71c1e2dcb8e16c453919cd3353c
Vinay khanagavi <116386393+Vinay-Khanagavi@users.noreply.github.com>
2024-11-28 23:17:38 +0530

    Changes to be committed: 	modified:   app/(root)/page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import { LandingPage } from "@/components/landingPage";
 import { ClerkProvider } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
